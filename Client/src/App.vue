ff4c8ac89fe79536e4e899370217589ba2538492
Colin Jones <1238372+ColsterJ@users.noreply.github.com>
2022-10-18 15:31:36 -0400

    Refactor form into MotionPictureForm

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref, watchEffect } from "vue";
 import MotionPictureTable from './components/MotionPictureTable.vue';
+import MotionPictureForm from './components/MotionPictureForm.vue';
 
-const refreshData = ref(true);
 const API_URL = 'https://localhost:7126/api/MotionPictures';
+const refreshData = ref(true);
 const currentList = ref([]);
 
 const showForm = ref(false);
@@ -11,7 +12,6 @@ const formMode = ref('add');
 
 watchEffect(async () => {
   if (refreshData.value === true) {
-    console.log("Trying to get data");
     currentList.value = await (await fetch(API_URL)).json()
     refreshData.value = false;
   }
@@ -50,25 +50,10 @@ function deleteRecord(id) {
       @delete-record="(id) => deleteRecord(id)"
     />
 
-    <div id="mp-edit-form" :class="{'hidden': !showForm}">
-      <h2 v-if="formMode === 'add'">Add a new movie</h2>
-      <h2 v-if="formMode === 'edit'">Edit a movie</h2>
-      <h2 v-if="formMode === 'copy'">Copy a movie</h2>
-      <form>
-        <div class="form-group">
-          <label for="nameControl">Name</label>
-          <input type="text" class="form-control" id="nameControl" placeholder="Enter title of movie">
-        </div>
-        <div class="form-group">
-          <label for="descriptionControl">Description</label>
-          <textarea class="form-control" id="descriptionControl" placeholder="Enter a description for the movie" />
-        </div>
-        <div class="form-group">
-          <label for="releaseYearControl">Release Year</label>
-          <input type="text" class="form-control" id="releaseYearControl" placeholder="Enter release year" />
-        </div>
-      </form>
-    </div>
+    <MotionPictureForm
+      :class="{'hidden': !showForm}"
+      :formMode="formMode"
+    />
   </div>
 </template>
 
---
+++ Client/src/components/MotionPictureForm.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps(['data','formMode'])
+// const emits = defineEmits(['add','edit','copy','delete-record'])
+</script>
+
+<template>
+  <div id="mp-edit-form">
+    <h2 v-if="formMode === 'add'">Add a new movie</h2>
+    <h2 v-if="formMode === 'edit'">Edit a movie</h2>
+    <h2 v-if="formMode === 'copy'">Copy a movie</h2>
+    <form>
+      <div class="form-group">
+        <label for="nameControl">Name</label>
+        <input type="text" class="form-control" id="nameControl" placeholder="Enter title of movie">
+      </div>
+
+      <div class="form-group">
+        <label for="descriptionControl">Description</label>
+        <textarea class="form-control" id="descriptionControl" placeholder="Enter a description for the movie" />
+      </div>
+      
+      <div class="form-group">
+        <label for="releaseYearControl">Release Year</label>
+        <input type="text" class="form-control" id="releaseYearControl" placeholder="Enter release year" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+</style>
