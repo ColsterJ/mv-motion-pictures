e447a956629eff1cad009327c0c19158026478fc
Colin Jones <1238372+ColsterJ@users.noreply.github.com>
2022-10-19 20:07:31 -0400

    Implement form validation & basic error messages and styling

--- Client/src/components/MotionPictureForm.vue
+++ Client/src/components/MotionPictureForm.vue
@@ -8,11 +8,15 @@ const name = ref('')
 const description = ref('')
 const releaseYear = ref(null)
 
+const formElement = ref(null)
+const attemptedToSubmit = ref(false);
+const formErrorList = ref([]);
+
 watchEffect(async () => {
   if (props.initialFormData !== null) {
     name.value = props.initialFormData.name;
     description.value = props.initialFormData.description;
-    releaseYear.value = props.initialFormData.releaseYear;
+    releaseYear.value = props.initialFormData.releaseYear.toString();
   } else {
     name.value = '';
     description.value = '';
@@ -21,16 +25,37 @@ watchEffect(async () => {
 })
 
 function save() {
-  console.log("Save clicked")
-  // TODO:
-  // 1. Validate form data (HTML5 Validation?)
-  // 2. If we have an editId, use it instead of null for the save-form event
+  attemptedToSubmit.value = true;
+
+  if (!formElement.value.checkValidity()) {
+    const errors = [];
+    if (name.value === '') {
+      errors.push('You must enter a name.');
+    }
+    if (releaseYear.value === '') {
+      errors.push('You must enter a release year.');
+    }
+    if (releaseYear.value.length !== 4 || (!/[0-9]{4}/.test(releaseYear.value))) {
+      errors.push('Release year must consist of 4 numbers (e.g. 1994).');
+    }
+
+    formErrorList.value = errors;
+
+    return;
+  }
+
   const formData = {
     name: name.value,
     description: description.value,
     releaseYear: releaseYear.value
   }
   emit('save-form', formData, props.idToUpdate);
+}
+
+function closeForm() {
+  attemptedToSubmit.value = false;
+  formErrorList.value = [];
+  emit('close-form');
 }
 
 </script>
@@ -40,29 +65,38 @@ function save() {
     <h2 v-if="formMode === 'add'">Add a new movie</h2>
     <h2 v-if="formMode === 'edit'">Edit a movie</h2>
     <h2 v-if="formMode === 'copy'">Copy a movie</h2>
-    <form>
+
+    <div v-if="formErrorList.length > 0">
+      Please address the following errors:
+      <p v-for="formError in formErrorList">* {{formError}}</p>
+    </div>
+
+    <form ref="formElement" :class="{'attempted-submit': attemptedToSubmit}">
       <div class="form-group">
-        <label for="nameControl">Name</label>
-        <input v-model="name" type="text" class="form-control" id="nameControl" placeholder="Enter title of movie">
+        <label for="nameControl">Name *</label>
+        <input required maxlength="50" v-model="name" type="text" class="form-control" id="nameControl" placeholder="Enter title of movie">
       </div>
 
       <div class="form-group">
         <label for="descriptionControl">Description</label>
-        <textarea v-model="description" class="form-control" id="descriptionControl" placeholder="Enter a description for the movie" />
+        <textarea maxlength="500" v-model="description" class="form-control" id="descriptionControl" placeholder="Enter a description for the movie" />
       </div>
       
       <div class="form-group">
-        <label for="releaseYearControl">Release Year</label>
-        <input v-model="releaseYear" type="text" class="form-control" id="releaseYearControl" placeholder="Enter release year" />
+        <label for="releaseYearControl">Release Year *</label>
+        <input required maxlength="4" pattern="[0-9]{4}" v-model="releaseYear" type="text" class="form-control" id="releaseYearControl" placeholder="Enter release year" />
       </div>
 
       <div class="form-group pt-4">
-        <button type="button" class="btn btn-primary mr-2" @click="save()">Save</button>
-        <button type="button" class="btn btn-secondary" @click="$emit('close-form')">Cancel</button>
+        <button type="submit" class="btn btn-primary mr-2" @click="save()" @submit="save()">Save</button>
+        <button type="button" class="btn btn-secondary" @click="closeForm()">Cancel</button>
       </div>
     </form>
   </div>
 </template>
 
 <style scoped>
+form.attempted-submit > div > input:invalid {
+  border: 1px solid red;
+}
 </style>
