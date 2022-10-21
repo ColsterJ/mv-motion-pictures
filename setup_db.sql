cd21c337e40a0090ca27ee8434acd094fff46195
Colin Jones <1238372+ColsterJ@users.noreply.github.com>
2022-10-20 20:40:22 -0400

    Prepopulate the table with some movie data

--- setup_db.sql
+++ setup_db.sql
@@ -89,3 +89,57 @@ CREATE TABLE [dbo].[MotionPictures](
 	[Release Year] [int] NOT NULL,
 ) ON [PRIMARY]
 GO
+
+INSERT INTO [dbo].[MotionPictures]
+           ([Name]
+           ,[Description]
+           ,[Release Year])
+     VALUES
+           ('The Matrix'
+           ,'When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.'
+           ,1999)
+
+INSERT INTO [dbo].[MotionPictures]
+           ([Name]
+           ,[Description]
+           ,[Release Year])
+     VALUES
+           ('Snowpiercer'
+           ,'In a future where a failed climate change experiment has killed all life except for the survivors who boarded the Snowpiercer (a train that travels around the globe), a new class system emerges.'
+           ,2013)
+
+INSERT INTO [dbo].[MotionPictures]
+           ([Name]
+           ,[Description]
+           ,[Release Year])
+     VALUES
+           ('Spider-Man: No Way Home'
+           ,'With Spider-Man''s identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man.'
+           ,2021)
+
+INSERT INTO [dbo].[MotionPictures]
+           ([Name]
+           ,[Description]
+           ,[Release Year])
+     VALUES
+           ('Big Fish'
+           ,'A frustrated son tries to determine the fact from fiction in his dying father''s life.'
+           ,2003)
+
+INSERT INTO [dbo].[MotionPictures]
+           ([Name]
+           ,[Description]
+           ,[Release Year])
+     VALUES
+           ('The Sandlot'
+           ,'In the summer of 1962, a new kid in town is taken under the wing of a young baseball prodigy and his rowdy team, resulting in many adventures.'
+           ,1993)
+
+INSERT INTO [dbo].[MotionPictures]
+           ([Name]
+           ,[Description]
+           ,[Release Year])
+     VALUES
+           ('Back to the Future'
+           ,'Marty McFly, a 17-year-old high school student, is accidentally sent 30 years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.'
+           ,1985)
